fb3125da3e2ed1c6088d270111a1f3605e103c60
Mako0530 <zhongtiancheng79@gmail.com>
2021-04-11 23:31:19 +0900

    商品情報を新規追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  extend Enumerize
+  enumerize :unit, in: [:yen, :usd]
+  mount_uploader :image, ImageUploader
+
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,10 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def accept_extensions
+    accept_list = extension_whitelist.map { |e| "image/#{e}" }
+    accept_list.join(",")
+  end
+end
