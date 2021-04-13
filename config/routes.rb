14d1d7dfa09e22ebbbc96cda19fb6a8a39059c11
Mako0530 <zhongtiancheng79@gmail.com>
2021-04-13 21:18:10 +0900

    管理者アカウント作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resource :basket
   resource :charge, only: %i(create)
-  resources :products, only: %i(new show create) do
+  resources :products, only: %i(show) do
     scope module: :products do
       resources :add_to_baskets, only: %i(create)
       resources :delete_in_baskets, only: %i(create)
     end
+  end
+  namespace :admins do
+    root to: "dashboards#index"
+    resources :products, only: %i(new create)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 20210413090203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
   create_table "basket_products", force: :cascade do |t|
     t.integer  "basket_id",  null: false
     t.integer  "product_id", null: false
@@ -32,6 +44,7 @@ ActiveRecord::Schema.define(version: 20210413090203) do
   end
 
   create_table "products", force: :cascade do |t|
+    t.integer  "admin_id",                    null: false
     t.string   "name",                        null: false
     t.text     "description",                 null: false
     t.integer  "price",                       null: false
@@ -39,6 +52,7 @@ ActiveRecord::Schema.define(version: 20210413090203) do
     t.string   "image",                       null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.index ["admin_id"], name: "index_products_on_admin_id", using: :btree
   end
 
   create_table "purchase_record_products", force: :cascade do |t|
@@ -76,6 +90,7 @@ ActiveRecord::Schema.define(version: 20210413090203) do
   add_foreign_key "basket_products", "baskets"
   add_foreign_key "basket_products", "products"
   add_foreign_key "baskets", "users"
+  add_foreign_key "products", "admins"
   add_foreign_key "purchase_record_products", "products"
   add_foreign_key "purchase_record_products", "purchase_records"
   add_foreign_key "purchase_records", "users"
